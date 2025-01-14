923f8d3f7286ee498a461ddd13d9fe3cf7c0c662
lucah101 <yinlingyi10101@163.com>
2025-01-14 15:26:56 +1300

    04-036

--- 01-tic-tac-toe-starting-project/src/App.jsx
+++ 01-tic-tac-toe-starting-project/src/App.jsx
@@ -1,16 +1,78 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/log";
+import { useState } from "react";
+import { WINNING_COMBINATIONS } from './winning-combinations.js';
+
+
+function deriveActivePLayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if(gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer;
+}
+
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, serActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePLayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+  let winner;
+
+  for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+      gameBoard[row][col] = player;
+  }
   
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if(firstSquareSymbol && 
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // serActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePLayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex} , player: currentPlayer }, 
+        ...prevTurns];
+
+      return updatedTurns;
+    });
+  }
 
   return <main>
     <div id="game-container">
-      <ol id="players">
-        <Player initialName="Player 1" symbol="X"/>
-        <Player initialName="Player 2" symbol="O"/>
+      <ol id="players" className="highlight-player">
+        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
+        <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
       </ol>
-      GAME BOARD
+      {winner && <p>You won, {winner}!</p>}
+      <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
     </div>
+    <Log turns={gameTurns} />
   </main>;
   
 }
