3b579e68cfe8031145c6e2c74d8d9414058824fd
lucah101 <yinlingyi10101@163.com>
2025-01-23 16:45:17 +1300

    1

--- 03-Refs and Portals/src/components/TimerChallenge.jsx
+++ 03-Refs and Portals/src/components/TimerChallenge.jsx
@@ -1,17 +1,35 @@
+import { useRef, useState } from "react";
+
 export default function TimerChallenge({title, targetTime}) {
+    const timer = useRef();
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimeExpired] = useState(false);
+
+    function handleStart() {
+        timer.current = setTimeout(() => {
+            setTimeExpired(true);
+        }, targetTime * 1000);
+        setTimerStarted(true);
+    }
+
+    function handleStop() {
+        clearTimeout(timer.current);
+    }
+
     return (
         <section className="challenge">
             <h2>{title}</h2>
+            {timerExpired && <p>You lost!</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? 'S' : ''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={handleStart}>
+                    {timerStarted ? 'stop' : 'Start'} Challenge
                 </button>
             </p>
-            <p className=""> 
-                Time is running... / Timer inactive
+            <p className={timerStarted ? 'active' : undefined}> 
+                {timerStarted ? 'Time is running..' : 'Timer inactive'}
             </p>
         </section>
     );
