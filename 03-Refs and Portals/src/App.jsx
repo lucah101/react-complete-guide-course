b1148a4366913d0fb3ab8c2707b2a1d69fc8d027
lucah101 <yinlingyi10101@163.com>
2025-01-27 14:59:19 +1300

    0127

--- 03-Refs and Portals/src/App.jsx
+++ 03-Refs and Portals/src/App.jsx
@@ -5,11 +5,12 @@ function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
-      <TimerChallenge title="Easy" targetTime={1}/>
-      <TimerChallenge title="Not easy" targetTime={5}/>
-      <TimerChallenge title="Getting tough" targetTime={10}/>
-      <TimerChallenge title="Pros only" targetTime={15}/>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1}/>
+        <TimerChallenge title="Not easy" targetTime={5}/>
+        <TimerChallenge title="Getting tough" targetTime={10}/>
+        <TimerChallenge title="Pros only" targetTime={15}/>
+      </div>
     </>
   );
 }
